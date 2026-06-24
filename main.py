856d4fda7babbfeb6ce099be0eceb0c26684c630
Danial <danazad2016@gmail.com>
2026-06-24 17:16:35 -0500

    refactor

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import time
 import pandas as pd
 from pathlib import Path
@@ -10,52 +11,24 @@ from watchdog.events import FileSystemEventHandler
 
 load_dotenv()
 
-TEMPLATE_PATH = os.getenv("TEMPLATE_DIRECTORY")
-REPORT_PATH = os.getenv("REPORT_DIRECTORY")
-DATA_PATH = os.getenv("DATA_DIRECTORY")
+DATA = os.getenv("DATA")
+FORMS = os.getenv("FORMS")
 
 class Main(FileSystemEventHandler):
-    def on_created(self, event):
-        try:
-            time.sleep(0.5)
+    def on_created(self, event):    
+        data = max(Path(DATA).glob("*.csv"), key = os.path.getmtime)
 
-            report_template = Document(list(Path(TEMPLATE_PATH).glob("*.docx"))[0])
-            report_title = None
-            test_data = {f"+{key} {sub_key}+": sub_value for key, value in pd.read_csv(max([file for file in Path(DATA_PATH).glob("*.csv") if file.is_file()], key = lambda file: file.stat().st_ctime), skiprows = [0,2], index_col = 0).astype(str).to_dict().items() for sub_key, sub_value in value.items()}
-
-
-            # finds and replaces the placeholders with the corresponding data
-            for row in template.tables[1].rows:
-                for cell in row.cells:
-                    for paragraph in cell.paragraphs:        
-                        for key, value in data.items():
-                            if key in paragraph.text:
-                                try:
-                                    value = f"{int(value):,}"
-                                except Exception:
-                                    print(Exception)
-
-                                new_text = paragraph.text.replace(key, value)
-                                paragraph.text = ""
-                                paragraph.text = new_text
-
-                                for run in paragraph.runs:
-                                    run.font.name = "Bookman Old Style"
-                                    run.font.size = Pt(10)
-
-                        if "+" in paragraph.text:
-                            paragraph.text = ""
-
-            template.save(str(Path(REPORT_PATH)) + report_title)
-        except Exception as exception:
-            print(exception)
+        print(data, flush = True)
 
 observer = Observer()
 
-observer.schedule(Main(), Path(DATA_PATH))
+observer.schedule(Main(), DATA)
 observer.start()
 
 try:
+    if observer.is_alive():
+        print("started", flush = True)
+
     while observer.is_alive():
         observer.join(1)
 finally:
