416272e8061e238df7f165a9070ef9a9ef01f95b
Danial <danazad2016@gmail.com>
2026-06-23 20:52:43 -0500

    clean up

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
+import os
+import time
+import pandas as pd
 from pathlib import Path
 from docx import Document
 from docx.shared import Pt
+from dotenv import load_dotenv
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
-from dotenv import load_dotenv
-import pandas as pd
-import os
 
 load_dotenv()
 
@@ -16,9 +17,14 @@ DATA_PATH = os.getenv("DATA_DIRECTORY")
 class Main(FileSystemEventHandler):
     def on_created(self, event):
         try:
-            template = Document(list(Path(TEMPLATE_PATH).glob("*.docx"))[0])
-            data = {f"+{key} {sub_key}+": sub_value for key, value in pd.read_csv(max([file for file in Path(DATA_PATH).glob("*.csv") if file.is_file()], key = lambda file: file.stat().st_ctime), skiprows = [0,2], index_col = 0).astype(str).to_dict().items() for sub_key, sub_value in value.items()}
+            time.sleep(0.5)
 
+            report_template = Document(list(Path(TEMPLATE_PATH).glob("*.docx"))[0])
+            report_title = None
+            test_data = {f"+{key} {sub_key}+": sub_value for key, value in pd.read_csv(max([file for file in Path(DATA_PATH).glob("*.csv") if file.is_file()], key = lambda file: file.stat().st_ctime), skiprows = [0,2], index_col = 0).astype(str).to_dict().items() for sub_key, sub_value in value.items()}
+
+
+            # finds and replaces the placeholders with the corresponding data
             for row in template.tables[1].rows:
                 for cell in row.cells:
                     for paragraph in cell.paragraphs:        
@@ -26,12 +32,13 @@ class Main(FileSystemEventHandler):
                             if key in paragraph.text:
                                 try:
                                     value = f"{int(value):,}"
-                                except ValueError:
-                                    pass
+                                except Exception:
+                                    print(Exception)
 
                                 new_text = paragraph.text.replace(key, value)
                                 paragraph.text = ""
                                 paragraph.text = new_text
+
                                 for run in paragraph.runs:
                                     run.font.name = "Bookman Old Style"
                                     run.font.size = Pt(10)
@@ -39,13 +46,13 @@ class Main(FileSystemEventHandler):
                         if "+" in paragraph.text:
                             paragraph.text = ""
 
-            template.save(str(Path(REPORT_PATH)) + r"\weld_report.docx")
+            template.save(str(Path(REPORT_PATH)) + report_title)
         except Exception as exception:
             print(exception)
 
-main = Main()
 observer = Observer()
-observer.schedule(main, Path(DATA_PATH))
+
+observer.schedule(Main(), Path(DATA_PATH))
 observer.start()
 
 try:
