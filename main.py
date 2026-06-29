7d799fa5df033e968cefe9da10cc1a8e3715e0e3
Danial Azadpour <danial@hurstlab.local>
2026-06-29 15:47:43 -0500

    add csv guard

--- main.py
+++ main.py
@@ -19,47 +19,48 @@ logging.basicConfig(
 class Main(FileSystemEventHandler):
     def on_created(self, event): 
         time.sleep(1)
-        logging.info(f"The following was created:\n> {event.src_path}")
+        logging.info(f"{Path(event.src_path).name} was created")
         
-        for report in Path(REPORTS_DIRECTORY).rglob("*.docx"):
-            if Path(report).stem.lower().replace(" ", "") == Path(event.src_path).stem.lower().replace(" ", ""):
-                try:
-                    template = Document(report)
+        if Path(event.src_path).suffix == ".csv":
+            for report in Path(REPORTS_DIRECTORY).rglob("*.docx"):
+                if Path(report).stem.lower().replace(" ", "") == Path(event.src_path).stem.lower().replace(" ", ""):
+                    try:
+                        template = Document(report)
 
-                    data = {f"+{key} {sub_key}+": sub_value for key, value in pd.read_csv(Path(event.src_path), skiprows = [0,2], index_col = 0).to_dict().items() for sub_key, sub_value in value.items()}
+                        data = {f"+{key} {sub_key}+": sub_value for key, value in pd.read_csv(Path(event.src_path), skiprows = [0,2], index_col = 0).to_dict().items() for sub_key, sub_value in value.items()}
 
-                    for row in template.tables[1].rows:
-                        for cell in row.cells:
-                            for paragraph in cell.paragraphs:        
-                                for key, value in data.items():
-                                    if key in paragraph.text:
-                                        if "stress" in key.lower():
-                                            value = int(value)
+                        for row in template.tables[1].rows:
+                            for cell in row.cells:
+                                for paragraph in cell.paragraphs:        
+                                    for key, value in data.items():
+                                        if key in paragraph.text:
+                                            if "stress" in key.lower():
+                                                value = int(value)
 
-                                            if value < 500000:
-                                                value = int(round(value / 100) * 100)
-                                            elif value == 500000 or value < 1000000:
-                                                value = int(round(value / 500) * 100)
-                                            else:
-                                                value = int(round(value / 1000) * 100)
+                                                if value < 500000:
+                                                    value = int(round(value / 100) * 100)
+                                                elif value == 500000 or value < 1000000:
+                                                    value = int(round(value / 500) * 100)
+                                                else:
+                                                    value = int(round(value / 1000) * 100)
 
-                                        if "elongation" in key or "reducation" in key:
-                                            value = round(int(value))
-                                            
-                                        new_text = paragraph.text.replace(key, f"{value:,}")
+                                            if "elongation" in key or "reducation" in key:
+                                                value = round(int(value))
+                                                
+                                            new_text = paragraph.text.replace(key, f"{value:,}")
+                                            paragraph.text = ""
+                                            paragraph.text = new_text
+
+                                            for run in paragraph.runs:
+                                                run.font.name = "Bookman Old Style"
+                                                run.font.size = Pt(10)
+
+                                    if "+" in paragraph.text:
                                         paragraph.text = ""
-                                        paragraph.text = new_text
 
-                                        for run in paragraph.runs:
-                                            run.font.name = "Bookman Old Style"
-                                            run.font.size = Pt(10)
-
-                                if "+" in paragraph.text:
-                                    paragraph.text = ""
-
-                    template.save(report)
-                except PermissionError:
-                    logging.info("permission error, still running the program observation")
+                        template.save(report)
+                    except PermissionError:
+                        logging.info("permission error, still running the program observation")
 
 if __name__ == "__main__":    
     load_dotenv()
