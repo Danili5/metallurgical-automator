f957d4a37cc2143f243ea6971c92166ea9b256d2
Danial <danazad2016@gmail.com>
2026-07-14 21:58:45 -0500

    clean up

--- main.py
+++ main.py
@@ -12,78 +12,104 @@ from watchdog.events import FileSystemEventHandler
 
 logging.basicConfig(
     level=logging.INFO,
-    format="[Time] %(asctime)s\n[Message] %(message)s\n",
+    format="> %(message)s at %(asctime)s.",
     datefmt="%H:%M:%S"
 )
 
 class Main(FileSystemEventHandler):
     def on_created(self, event): 
-        time.sleep(2)
-        logging.info(f"{Path(event.src_path).name} was created")
-        
-        if Path(event.src_path).suffix == ".csv":
+        if Path(event.src_path).is_file() and Path(event.src_path).suffix == '.csv' and '_2' not in Path(event.src_path).stem:
             data_file = Path(event.src_path)
-            new_data_file = None
 
-            print(data_file, flush = True)
+            # a method to verify that the csv data file is loaded before reading it.
+            if self._check(data_file) == 0:
+                return
 
-            if '_1' in data_file.name:
-                stem = data_file.stem.replace('_1', '')
-                extension = data_file.suffix
-                base = data_file.parent
+            logging.info(pd.read_csv(data_file))
 
-                new_data_file = data_file.rename(base / f'{stem}{extension}')
+    def _check(self, data_file, max_attemps = 20):
+        attempts = 0
 
-            for report in Path(REPORTS_DIRECTORY).rglob("*.docx"):
-                if Path(report).stem.lower().replace(" ", "") == new_data_file.stem.lower().replace(" ", ""):
-                    try:
-                        template = Document(report)
-                        data = {f"+{key} {sub_key}+": sub_value for key, value in pd.read_csv(new_data_file, skiprows = [0,2], index_col = 0).to_dict().items() for sub_key, sub_value in value.items()}
+        while attempts < max_attemps:
+            try:
+                with open(data_file, 'r+'):
+                    return 1
+            except (PermissionError, FileNotFoundError, IOError):
+                attempts += 1
+                time.sleep(0.5)
 
-                        for row in template.tables[1].rows:
-                            for cell in row.cells:
-                                for paragraph in cell.paragraphs:        
-                                    for key, value in data.items():
-                                        if key in paragraph.text:
-                                            if "stress" in key.lower():
-                                                value = int(value)
+        return 0
+        
 
-                                                if value < 500000:
-                                                    value = int(round(value / 100) * 100)
-                                                elif value == 500000 or value < 1000000:
-                                                    value = int(round(value / 500) * 100)
-                                                else:
-                                                    value = int(round(value / 1000) * 100)
+            
 
-                                            if "elongation" in key or "reducation" in key:
-                                                value = round(int(value))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # time.sleep(1)
+        # logging.info(f"{Path(event.src_path).name} was created")
+        
+        # if Path(event.src_path).suffix == ".csv":
+        #     for report in Path(REPORTS_DIRECTORY).rglob("*.docx"):
+        #         if Path(report).stem.lower().replace(" ", "") == Path(event.src_path).stem.lower().replace(" ", ""):
+        #             try:
+        #                 template = Document(report)
+
+        #                 data = {f"+{key} {sub_key}+": sub_value for key, value in pd.read_csv(Path(event.src_path), skiprows = [0,2], index_col = 0).to_dict().items() for sub_key, sub_value in value.items()}
+
+        #                 for row in template.tables[1].rows:
+        #                     for cell in row.cells:
+        #                         for paragraph in cell.paragraphs:        
+        #                             for key, value in data.items():
+        #                                 if key in paragraph.text:
+        #                                     if "stress" in key.lower():
+        #                                         value = int(value)
+
+        #                                         if value < 500000:
+        #                                             value = int(round(value / 100) * 100)
+        #                                         elif value == 500000 or value < 1000000:
+        #                                             value = int(round(value / 500) * 100)
+        #                                         else:
+        #                                             value = int(round(value / 1000) * 100)
+
+        #                                     if "elongation" in key or "reducation" in key:
+        #                                         value = round(int(value))
                                                 
-                                            new_text = paragraph.text.replace(key, f"{value:,}")
-                                            paragraph.text = ""
-                                            paragraph.text = new_text
+        #                                     new_text = paragraph.text.replace(key, f"{value:,}")
+        #                                     paragraph.text = ""
+        #                                     paragraph.text = new_text
 
-                                            for run in paragraph.runs:
-                                                run.font.name = "Bookman Old Style"
-                                                run.font.size = Pt(10)
+        #                                     for run in paragraph.runs:
+        #                                         run.font.name = "Bookman Old Style"
+        #                                         run.font.size = Pt(10)
 
-                                    if "+" in paragraph.text:
-                                        paragraph.text = ""
+        #                             if "+" in paragraph.text:
+        #                                 paragraph.text = ""
 
-                        template.save(report)
-                    except PermissionError:
-                        logging.info("permission error, still running the program observation")
+        #                 template.save(report)
+        #             except PermissionError:
+        #                 logging.info("permission error, still running the program observation")
 
 if __name__ == "__main__":    
     load_dotenv()
 
     DATA_DIRECTORY = os.getenv("DATA_DIRECTORY")
     REPORTS_DIRECTORY = os.getenv("REPORTS_DIRECTORY")
-
-    if not DATA_DIRECTORY or not REPORTS_DIRECTORY:
-        logging.error("A directory is missing in .env")
-        sys.exit(1)
     
-    logging.info(f"Started observation on the following directories:\n> {DATA_DIRECTORY}\n> {REPORTS_DIRECTORY}")
+    logging.info(f"observation has begun")
 
     observer = Observer()
 
